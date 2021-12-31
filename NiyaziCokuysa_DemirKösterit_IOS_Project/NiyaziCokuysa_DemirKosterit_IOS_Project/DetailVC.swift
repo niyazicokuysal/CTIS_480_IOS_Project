a71e49aeff4207c6d4cfb555c44f64e1123975d1
Demir Kosterit <demirkosterit@gmail.com>
2021-12-31 22:40:29 +0300

    Detail VC DetailVC Added

---
+++ NiyaziCokuysa_DemirKösterit_IOS_Project/NiyaziCokuysa_DemirKosterit_IOS_Project/DetailVC.swift
@@ -0,0 +1,17 @@
+//
+//  DetailVC.swift
+//  NiyaziCokuysa_DemirKosterit_IOS_Project
+//
+//  Created by CTIS Student on 31.12.2021.
+//  Copyright © 2021 niyazi-demir. All rights reserved.
+//
+
+import UIKit
+
+class DetailVC: UIViewController {
+    @IBOutlet weak var mImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
